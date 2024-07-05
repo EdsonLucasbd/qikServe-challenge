84a2cfc2d39bc96e04e91da007d340fee1ff0999
Edson Lucas <47072312+EdsonLucasbd@users.noreply.github.com>
2024-07-05 18:34:54 -0300

    feat: api request and hero banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
-import './App.css'
+import { useQuery } from '@tanstack/react-query'
+import { Layout } from './components/Layout'
+import { Venue } from './types/venue'
+
+async function getRestaurantDetails() {
+  return (await fetch(`${import.meta.env.VITE_API_URL}/challenge/venue/9`).then((res) =>
+    res.json()
+  )) as Venue
+}
+
+function useRestaurantDetails() {
+  const query = useQuery<Venue>({ queryKey: ['RestaurantDetails'], queryFn: getRestaurantDetails })
+
+  return query
+}
 
 function App() {
+  const { data, isPending, isError, error } = useRestaurantDetails()
+
+  if (isPending) {
+    return <span>Loading...</span>
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>
+  }
 
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Layout headerColor={data?.webSettings.navBackgroundColour}>
+      <div className='w-full h-[158px] bg-center bg-cover bg-no-repeat' style={{ backgroundImage: `url(${data?.webSettings.bannerImage})` }}></div>
+    </Layout>
   )
 }
 
