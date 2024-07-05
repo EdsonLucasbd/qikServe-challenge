ea8bfe429af12269783fa89b7ee4138175b79bd0
Edson Lucas <47072312+EdsonLucasbd@users.noreply.github.com>
2024-07-05 18:24:43 -0300

    feat: configure tanstack query

---
+++ src/providers/ReactQueryProvider.tsx
@@ -0,0 +1,23 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { useState } from 'react'
+
+export function ReactQueryProviders(props: { children: React.ReactNode }) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      }),
+  )
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {props.children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
