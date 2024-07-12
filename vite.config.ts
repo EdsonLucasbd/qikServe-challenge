2ba85412d39d93f7c02e1aaaed9132abff5f8bd4
Edson Lucas <47072312+EdsonLucasbd@users.noreply.github.com>
2024-07-12 14:59:26 -0300

    fix: config to cors in prod

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,35 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://cdn-dev.preoday.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+export default defineConfig(({
+  mode
+}) => {
+  const config = {
+    plugins: [react()],
+    server: {}
+  };
+
+  if (mode === 'development') {
+    config.server = {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5173',
+          changeOrigin: true,
+          rewrite: (path: string) => path.replace(/^\/api/, '')
+        }
       }
-    }
+    };
+  } else {
+    config.server = {
+      proxy: {
+        '/api': {
+          target: 'https://seu-proxy-em-producao.com',
+          changeOrigin: true,
+          rewrite: (path: string) => path.replace(/^\/api/, ''),
+        },
+      },
+    };
   }
+
+  return config
 })
