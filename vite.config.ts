cd5b8fcb6e683224ef21cd6c42c7c8668a7efdfc
Edson Lucas <47072312+EdsonLucasbd@users.noreply.github.com>
2024-07-13 09:06:25 -0300

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://cdn-dev.preoday.com',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
+export default defineConfig(({mode}) => {
+  const config = {
+    plugins: [react()],
+    server: {}
+  };
+
+  if(mode === 'development') {
+    config.server = {
+      proxy: {
+        '/api': {
+          target: 'https://cdn-dev.preoday.com',
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path: string) => path.replace(/^\/api/, '')
+        }
+      }
+    }
+  } else {
+    config.server = {
+      proxy: {
+        '/*': {
+          target: 'https://cdn-dev.preoday.com',
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path: string) => path.replace(/^\/api/, '')
+        }
       }
     }
   }
+
+  return config
 })
