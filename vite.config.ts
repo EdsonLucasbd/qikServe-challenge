e62a38badd6c2cb38ab958f43d5bd22628ed61ee
Edson Lucas <47072312+EdsonLucasbd@users.noreply.github.com>
2024-07-13 08:40:37 -0300

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,15 @@ export default defineConfig({
         target: 'https://cdn-dev.preoday.com',
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/([^/]*\/)api([^/]*)/, '$1/$2')
+        rewrite: (path) => {
+          if (path.startsWith('/api/challenge/venue')) {
+            return path.replace('/api/challenge/venue', '/challenge/venue');
+          } else if (path.startsWith('/api/challenge/menu')) {
+            return path.replace('/api/challenge/menu', '/challenge/menu');
+          } else {
+            return path;
+          }
+        }
       }
     }
   }
