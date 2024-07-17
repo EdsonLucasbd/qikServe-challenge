d47683abc3307dbbf88f4d66699ddce2fa88706f
Edson Lucas <47072312+EdsonLucasbd@users.noreply.github.com>
2024-07-17 07:53:54 -0300

    style: add scroll area in mobile cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -2,6 +2,7 @@ import { Minus, Plus } from "@phosphor-icons/react"
 import { useCart } from "../features/cart/hooks/useCart"
 import { useWebSettings } from "../features/webSettings/hooks/useWebSettings"
 import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog"
+import { ScrollArea } from "./ui/scroll-area"
 // import { useEffect, useState } from "react"
 
 export const Cart = () => {
@@ -85,46 +86,48 @@ export const Cart = () => {
                 </button>
               </div>
             </DialogTrigger>
-            <DialogContent className="p-0">
+            <DialogContent className="p-0 h-full">
               <DialogHeader className="py-[23px] bg-white">
                 <DialogTitle className="font-medium text-lg text-color-dark pb-5 border-b border-b-[#DADADA]">Basket</DialogTitle>
               </DialogHeader>
-              <div className="flex flex-col w-full h-full divide-y divide-[#DADADA] bg-white">
-                {cart.items.map((item) => (
-                  <div className="flex flex-col pt-2" key={item.id}>
-                    <div className="flex flex-col px-4">
-                      <div className="flex items-center justify-between mb-1">
-                        <p className="text-base text-color-dark">{item.name}</p>
-                        <p className="text-base font-medium">R${item.price.toFixed(2)}</p>
+              <div className="flex flex-col w-full h-full bg-white">
+                <ScrollArea className={`${cart.items.length < 2 ? 'h-[267px]' : 'h-[80%]'}`}>
+                  {cart.items.map((item) => (
+                    <div className={`flex flex-col pt-2 even:border-b border-[#DADADA] ${cart.items.length > 1 ? 'odd:border-b' : ''}`} key={item.id}>
+                      <div className="flex flex-col px-4">
+                        <div className="flex items-center justify-between mb-1">
+                          <p className="text-base text-color-dark">{item.name}</p>
+                          <p className="text-base font-medium">R${item.price.toFixed(2)}</p>
+                        </div>
+                        <p className={`${item.option ? 'flex' : 'hidden'} text-base text-color-light-gray`}>{item.option}</p>
                       </div>
-                      <p className={`${item.option ? 'flex' : 'hidden'} text-base text-color-light-gray`}>{item.option}</p>
-                    </div>
-                    <div className="flex gap-x-1 items-center justify-between p-2 w-[30%] ml-2 mb-4">
-                      <button
-                        className="flex justify-center items-center size-5 border-none rounded-full"
-                        onClick={() => cart.updateCartItemQuantity(item.id, item.quantity - 1)}
-                        style={{
-                          backgroundColor: webSettings?.primaryColour
-                        }}
-                      >
-                        <Minus size={12} className="text-white w-[18px]" weight="bold" />
-                      </button>
+                      <div className="flex gap-x-1 items-center justify-between p-2 w-[30%] ml-2 mb-4">
+                        <button
+                          className="flex justify-center items-center size-5 border-none rounded-full"
+                          onClick={() => cart.updateCartItemQuantity(item.id, item.quantity - 1)}
+                          style={{
+                            backgroundColor: webSettings?.primaryColour
+                          }}
+                        >
+                          <Minus size={12} className="text-white w-[18px]" weight="bold" />
+                        </button>
 
-                      <span className="font-bold text-base">{item.quantity}</span>
+                        <span className="font-bold text-base">{item.quantity}</span>
 
-                      <button
-                        className="flex justify-center items-center size-5 border-none rounded-full"
-                        onClick={() => cart.updateCartItemQuantity(item.id, item.quantity + 1)}
-                        style={{
-                          backgroundColor: webSettings?.primaryColour
-                        }}
-                      >
-                        <Plus size={12} className="text-white w-[18px]" weight="bold" />
-                      </button>
+                        <button
+                          className="flex justify-center items-center size-5 border-none rounded-full"
+                          onClick={() => cart.updateCartItemQuantity(item.id, item.quantity + 1)}
+                          style={{
+                            backgroundColor: webSettings?.primaryColour
+                          }}
+                        >
+                          <Plus size={12} className="text-white w-[18px]" weight="bold" />
+                        </button>
+                      </div>
                     </div>
-                  </div>
-                ))}
-                <div className="flex h-full p-4 pb-96 w-full items-center justify-between bg-[#F8F9FA] border-t border-t-[#DADADA]">
+                  ))}
+                </ScrollArea>
+                <div className="flex h-full p-4 pb-32 w-full items-start justify-between bg-[#F8F9FA] border-t border-t-[#DADADA]">
                   <p className="text-2xl font-light text-color-dark">Total:</p>
                   <p className="text-2xl font-bold text-color-dark">R${cart.totalPrice.toFixed(2)}</p>
                 </div>
